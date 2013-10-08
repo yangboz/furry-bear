09f416b08d457495f74cda8deb20434eea271cac
yangboz <YoungWelle@gmail.com>
2013-10-08 11:30:35 +0800

    CHANGED:predefined the ComplexReviewViewController for the table view definition inside of storyboard design;

---
+++ FrontEnd/FurryBear_iOS/FurryBear_iOS/ComplexReviewViewController.h
@@ -0,0 +1,13 @@
+//
+//  ComplexReviewViewController.h
+//  FurryBear_iOS
+//
+//  Created by yangboz on 13-10-8.
+//  Copyright (c) 2013年 GODPAPER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ComplexReviewViewController : UIViewController <UITableViewDataSource,UITableViewDelegate>
+
+@end
