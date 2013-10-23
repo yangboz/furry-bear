e0f9457a7c5cdeee0a014cd4c87626675be3af68
yangboz <YoungWelle@gmail.com>
2013-10-23 08:53:50 +0800

    ADDED:standalone SEGUE tableview controllers

---
+++ FrontEnd/FurryBear_iOS/FurryBear_iOS/VC_SegueImageUpload.h
@@ -0,0 +1,13 @@
+//
+//  VC_SegueImageUpload.h
+//  FurryBear_iOS
+//
+//  Created by yangboz on 13-10-23.
+//  Copyright (c) 2013年 GODPAPER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VC_SegueImageUpload : UITableViewController
+
+@end
