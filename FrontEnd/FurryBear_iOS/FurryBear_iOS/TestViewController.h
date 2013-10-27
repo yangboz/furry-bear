13e3a6a224a95ef3f8d33971deaa111f5ed7a8df
yangboz <YoungWelle@gmail.com>
2013-10-27 12:09:56 +0800

    CHANGED:applied the upload image function at relevant segue

---
+++ FrontEnd/FurryBear_iOS/FurryBear_iOS/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  FurryBear_iOS
+//
+//  Created by yangboz on 13-10-27.
+//  Copyright (c) 2013年 GODPAPER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UITableViewController
+
+@end
--- FrontEnd/FurryBear_iOS/FurryBear_iOS/VC_SegueImageUpload.m
+++ FrontEnd/FurryBear_iOS/FurryBear_iOS/VC_SegueImageUpload.m
@@ -50,7 +50,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
+    return 0;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
