e40cc2c51f0549842e50385ea65edd59c31728b5
yangboz <YoungWelle@gmail.com>
2014-03-22 11:38:17 +0800

    CHANGED:minor modification on FeatureItemCell

--- FrontEnd/FurryBear_iOS/FurryBear_iOS/VC_FeatureCateItems.m
+++ FrontEnd/FurryBear_iOS/FurryBear_iOS/VC_FeatureCateItems.m
@@ -421,12 +421,12 @@
 - (NSString *)symbolForRating:(int)rating
 {
     switch (rating) {
-        case 0: return @",☆";
-        case 1: return @",★";
-        case 2: return @",★★";
-        case 3: return @",★★★";
-        case 4: return @",★★★★";
-        case 5: return @",★★★★★";
+        case 0: return @"☆";
+        case 1: return @"★";
+        case 2: return @"★★";
+        case 3: return @"★★★";
+        case 4: return @"★★★★";
+        case 5: return @"★★★★★";
     }
     return nil;
 }
