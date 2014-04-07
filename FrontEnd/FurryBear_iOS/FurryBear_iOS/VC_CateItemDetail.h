80f298d040d21bcc5973dc2994ed64d147aa3e39
yangboz <YoungWelle@gmail.com>
2014-04-07 10:07:46 +0800

    CHANGED:applied the prototype of VC_CateItemDetail

---
+++ FrontEnd/FurryBear_iOS/FurryBear_iOS/VC_CateItemDetail.h
@@ -0,0 +1,13 @@
+//
+//  VC_CateItemDetail.h
+//  FurryBear_iOS
+//
+//  Created by yangboz on 14-4-7.
+//  Copyright (c) 2014年 GODPAPER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VC_CateItemDetail : UITableViewController
+
+@end
